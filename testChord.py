fccacfb9cb48100e14ac941905597f1f01824f1e
Hiroki Yamada <gnik_orih@yahoo.co.jp>
2024-02-08 16:52:39 +0900

    fix

--- testChord.py
+++ testChord.py
@@ -17,8 +17,8 @@ https://docs.microsoft.com/en-us/azure/cognitive-services/cognitive-services-sec
 endpoint = "https://japaneast.api.cognitive.microsoft.com/"
 key = "40fe3d4281b34560817dcaf4a1f1822a"
 
-model_id = "domesticDirect-Format2-v5"
-formUrl = "https://harmonious-snickerdoodle-2d1128.netlify.app/test.pdf"
+model_id = "domesticDirect-Format2-v7"
+formUrl = "https://harmonious-snickerdoodle-2d1128.netlify.app//BOX20021624_20231206.pdf"
 
 document_analysis_client = DocumentAnalysisClient(
     endpoint=endpoint, credential=AzureKeyCredential(key)
@@ -33,37 +33,45 @@ for idx, document in enumerate(result.documents):
     print("Document has type {}".format(document.doc_type))
     print("Document has confidence {}".format(document.confidence))
     print("Document was analyzed by model with ID {}".format(result.model_id))
+    data_container = {}
     for name, field in document.fields.items():
         field_value = field.value if field.value else field.content
-        print("......found field of type '{}' with value '{}' and with confidence {}".format(field.value_type, field_value, field.confidence))
+        # print("......found field of type '{}' with value '{}' and with confidence {}".format(field.value_type, field_value, field.confidence))
+        print("{}:'{}',".format(name ,field_value))
+        data_container[name] = field_value
+
+print(data_container) 
+    
 
 
-# iterate over tables, lines, and selection marks on each page
-for page in result.pages:
-    print("\nLines found on page {}".format(page.page_number))
-    for line in page.lines:
-        print("...Line '{}'".format(line.content.encode('utf-8')))
-    for word in page.words:
-        print(
-            "...Word '{}' has a confidence of {}".format(
-                word.content.encode('utf-8'), word.confidence
-            )
-        )
-    for selection_mark in page.selection_marks:
-        print(
-            "...Selection mark is '{}' and has a confidence of {}".format(
-                selection_mark.state, selection_mark.confidence
-            )
-        )
 
-for i, table in enumerate(result.tables):
-    print("\nTable {} can be found on page:".format(i + 1))
-    for region in table.bounding_regions:
-        print("...{}".format(i + 1, region.page_number))
-    for cell in table.cells:
-        print(
-            "...Cell[{}][{}] has content '{}'".format(
-                cell.row_index, cell.column_index, cell.content.encode('utf-8')
-            )
-        )
+
+# # iterate over tables, lines, and selection marks on each page
+# for page in result.pages:
+#     print("\nLines found on page {}".format(page.page_number))
+#     for line in page.lines:
+#         print("...Line '{}'".format(line.content.encode('utf-8')))
+#     for word in page.words:
+#         print(
+#             "...Word '{}' has a confidence of {}".format(
+#                 word.content.encode('utf-8'), word.confidence
+#             )
+#         )
+#     for selection_mark in page.selection_marks:
+#         print(
+#             "...Selection mark is '{}' and has a confidence of {}".format(
+#                 selection_mark.state, selection_mark.confidence
+#             )
+#         )
+
+# for i, table in enumerate(result.tables):
+#     print("\nTable {} can be found on page:".format(i + 1))
+#     for region in table.bounding_regions:
+#         print("...{}".format(i + 1, region.page_number))
+#     for cell in table.cells:
+#         print(
+#             "...Cell[{}][{}] has content '{}'".format(
+#                 cell.row_index, cell.column_index, cell.content.encode('utf-8')
+#             )
+#         )
 print("-----------------------------------")
